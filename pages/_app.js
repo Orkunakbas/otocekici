25b09caec09be0ce11e32bca605a9409a99b0606
orkunakbas <orkunakbas1o@gmail.com>
2025-11-03 15:32:02 +0300

    s

--- pages/_app.js
+++ pages/_app.js
@@ -5,11 +5,24 @@ import { store } from '@/store/store'
 import { Toaster } from 'react-hot-toast'
 import Navbar from '@/components/menu/Navbar'
 import MobileFixedButtons from '@/components/MobileFixedButtons'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }) {
   return (
     <Provider store={store}>
       <HeroUIProvider>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17684011350"
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);} 
+            gtag('js', new Date());
+            gtag('config', 'AW-17684011350');
+          `}
+        </Script>
         <Navbar />
         <Component {...pageProps} />
         <MobileFixedButtons />
